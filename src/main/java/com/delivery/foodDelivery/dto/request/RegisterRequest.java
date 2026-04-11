c6eaa3edf6e997cdb7e5e9f1d15ec87e6be34299
Priyanshu <pv254424@gmail.com>
2026-04-11 23:04:12 +0530

    register request with db

--- src/main/java/com/delivery/foodDelivery/dto/request/RegisterRequest.java
+++ src/main/java/com/delivery/foodDelivery/dto/request/RegisterRequest.java
@@ -1,8 +1,32 @@
 package com.delivery.foodDelivery.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100)
+    private String name;
+
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "Phone is required")
+    @Pattern(regexp = "^[6-9]\\d{9}$", message = "Invalid Indian phone number")
+    private String phone;
+
+    private String address;
+
+    private String role;
 }
