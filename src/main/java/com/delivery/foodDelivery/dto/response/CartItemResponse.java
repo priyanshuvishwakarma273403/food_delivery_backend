f84cdef0c9092b2306edcc522eb5046bce0a7edd
Priyanshu <pv254424@gmail.com>
2026-04-11 23:42:14 +0530

    cart item response in db

---
+++ src/main/java/com/delivery/foodDelivery/dto/response/CartItemResponse.java
@@ -0,0 +1,19 @@
+package com.delivery.foodDelivery.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CartItemResponse {
+    private Long id;
+    private Long menuItemId;
+    private String menuItemName;
+    private String menuItemImage;
+    private Double unitPrice;
+    private Integer quantity;
+    private Double subtotal;
+
+}
