511474c612964632be2473bf73c369d2bd17cf32
Priyanshu <pv254424@gmail.com>
2026-04-22 01:15:38 +0530

    Fix: Add missing ResponseEntity import in AiService

--- src/main/java/com/delivery/foodDelivery/service/AiService.java
+++ src/main/java/com/delivery/foodDelivery/service/AiService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
