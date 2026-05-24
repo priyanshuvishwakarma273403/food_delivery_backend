588b7c58a00ab661139d34f4480662921e3a85e9
Priyanshu <pv254424@gmail.com>
2026-05-24 18:13:10 +0530

    feat: implement ReviewController to handle adding and retrieving restaurant and menu item reviews

--- src/main/java/com/delivery/foodDelivery/controller/ReviewController.java
+++ src/main/java/com/delivery/foodDelivery/controller/ReviewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reviews")
+@RequestMapping("/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
